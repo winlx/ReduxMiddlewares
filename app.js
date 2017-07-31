e7b723a8a18e43bc346d4958d3a50da5302293c4
Aleksei Iakovets <aleksei@winlx.com>
2017-07-31 14:40:07 +0300

    Self-made promise middleware

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const { createStore } = require('redux');
+const { createStore, applyMiddleware } = require('redux');
 
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 
 function increment(amount) {
-  return {
-    type: INCREMENT,
-    amount,
-  };
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        type: INCREMENT,
+        amount,
+      });
+    }, 1000);
+  });
 }
 
 function decrement(amount) {
@@ -32,47 +36,16 @@ function reducer(state = initialState, action) {
   }
 }
 
-const addTriple = store => next => (action) => {
-  action.amount *= 3;
+const promise = store => next => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+
   return next(action);
 };
 
-const addDouble = store => next => (action) => {
-  action.amount *= 2;
-  return next(action);
-};
+const store = createStore(reducer, applyMiddleware(promise));
 
-const addLog = store => next => (action) => {
-  console.log('State before:', store.getState());
-  console.log('Action:', action);
-
-  const result = next(action);
-
-  console.log('State after:', store.getState());
-
-  return result;
-};
-
-function applyMiddleware(...middlewares) {
-  return createStore => (reducer, initialState, enhancer) => {
-    const store = createStore(reducer, initialState, enhancer);
-
-    let dispatch = store.dispatch;
-    let chain = [];
-
-    const middlewareAPI = {
-      getState: store.getState,
-      dispatch: action => dispatch(action),
-    };
-
-    chain = middlewares.map(middleware => middleware(middlewareAPI));
-    // chain.reduce аналог compose из Redux
-    dispatch = chain.reduce((f1, f2) => (...args) => f1(f2(...args)))(store.dispatch);
-
-    return Object.assign({}, store, { dispatch });
-  };
-}
-
-const store = createStore(reducer, applyMiddleware(addLog, addDouble, addTriple));
+store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(increment(3));
